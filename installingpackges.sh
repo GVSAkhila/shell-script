81b374ca23d25b413d0720bccd0a473b73fca44a
sankar <sankargv17999@gmail.com>
2024-05-03 14:23:44 +0530

    packages

--- installingpackges.sh
+++ installingpackges.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# our program goal is to install mysql
+
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
-echo" Error:please run this script with root user"
-exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
 fi
-yum install musql -y
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y
