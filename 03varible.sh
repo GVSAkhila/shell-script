e09716490f5939ece5c403263f6cdc24e59c2dda
sankar <sankargv17999@gmail.com>
2024-05-03 10:42:21 +0530

    vribles

---
+++ 03varible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# printing username and password
+
+echo "Enter the username"
+
+read USERNAME
+echo  "username entered: $USERNAME"
+
+echo "enter the password"
+
+read PASSWORD
+
+echo "password is entered: $PASSWORD"
