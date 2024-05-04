cbb9ca8a9320d42bc1432d78ab7db486f2a31727
sankar <sankargv17999@gmail.com>
2024-05-04 14:45:43 +0530

    functions

--- 01functions.sh
+++ 01functions.sh
@@ -3,28 +3,32 @@
 USERID=$(id -u)
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE="/tmp/$SCRIPT_NAME-$DATE.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-then
-echo -e "$2 $R Installation fail"
-else 
-echo -e "$2 $G installation sucess"
-fi
-}
+# Define color variables
 G="\e[32m"
 R="\e[31m"
+NC="\e[0m"  # Reset color
 
-if [[ $USERID -ne 0 ]]
-then
-    echo -e "$R Please run this script with root user."
+# Function to validate command execution
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "${R} $2 Installation failed${NC}"
+        exit 1
+    else 
+        echo -e "${G} $2 Installation succeeded${NC}"
+    fi
+}
+
+# Check if the script is run as root
+if [[ $USERID -ne 0 ]]; then
+    echo -e "${R} Please run this script with root user.${NC}"
     exit 1
 fi
 
-for i in $@
-do
-echo "$i"
-VALIDATE $? "$i"
+# Loop through each argument passed to the script and execute it
+for i in "$@"; do
+    echo "Executing: $i"
+    eval "$i" &>> "$LOGFILE"
+    VALIDATE $? "$i"
 done
-
