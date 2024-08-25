6ee7da426034477bbb329fba92df2960102d3962
Shankar <sankargv17999@gmail.com>
2024-08-25 13:51:40 +0530

    varaibles

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "sankar": HI "Gv" how are  you ?
+echo  "GV"    : HI "sankar", I am good thanks for asking
+echo  "sankar : I am learning devops in join devops.com institute
+echo   "GV"   : is it how it is "sankar"
