01ec4fb27c92a6394e476ff799e4ee4ea74eb5cc
Shankar <sankargv17999@gmail.com>
2024-08-25 16:27:24 +0530

    varaibles

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$1": HI "$2" how are  you ?
+echo  "$2"    : HI "$1", I am good thanks for asking
+echo  "$1": I am learning devops in join devops.com institute
+echo  "$2"   : is it how it is "$1"
