9b67b8995a4d9d750f6e3df7836c6eabb5204b80
Shankar <sankargv17999@gmail.com>
2024-08-26 23:23:15 +0530

    condtions

--- 05-condtions.sh
+++ 05-condtions.sh
@@ -3,6 +3,9 @@
 # Get the current day of the week (e.g., Sun, Mon, Tue, etc.)
 Day=$1
 
+# Print the received value to debug
+echo "Received day: '$Day'"
+
 # Check if today is Sunday
 if [ "$Day" == "Sunday" ]; then
     echo "No class today."
