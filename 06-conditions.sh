9acfa12333d7162b5095ceb4708dac350778561b
Shankar <sankargv17999@gmail.com>
2024-08-26 23:57:59 +0530

    installations

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -6,7 +6,7 @@ number=$1
 
 if [ $number -gt 20 ] 
 then
-    echo "Given number is greaterthan 20"
+    echo "Given $number: is greaterthan 20"
 else
-     echo "Given number is lessthan 20"
+     echo "Given $number: is lessthan 20"
 fi
