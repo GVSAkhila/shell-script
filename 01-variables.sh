25ef8812443a25ff72e672e8b9719c28bcd198e0
Shankar <sankargv17999@gmail.com>
2024-08-26 21:36:57 +0530

    Date commands

--- 01-variables.sh
+++ 01-variables.sh
@@ -3,7 +3,7 @@
 person1=$1
 person2=$2
 
-echo "$1": HI "$2" how are  you ?
+echo  "$1"    : HI "$2" how are  you ?
 echo  "$2"    : HI "$1", I am good thanks for asking
-echo  "$1": I am learning devops in join devops.com institute
-echo  "$2"   : is it how it is "$1"
+echo  "$1"    : I am learning devops in join devops.com institute
+echo  "$2"    : is it how it is "$1"
