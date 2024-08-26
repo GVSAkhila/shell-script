eab795e497228fb89c0a33c2b5a2470eae799b9d
Shankar <sankargv17999@gmail.com>
2024-08-26 23:20:22 +0530

    condtions

--- 05-condtions.sh
+++ 05-condtions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get the current day of the week (e.g., Sun, Mon, Tue, etc.)
-today=$1
+Day=$1
 
 # Check if today is Sunday
-if [ "$today" == "Sunday" ]; then
+if [ "$Day" == "Sunday" ]; then
     echo "No class today."
     exit 1
 else
