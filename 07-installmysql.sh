169a5859ad3db3741f001c7d5ef4e6a9a29f62be
Shankar <sankargv17999@gmail.com>
2024-08-27 00:05:05 +0530

    installations

--- 07-installmysql.sh
+++ 07-installmysql.sh
@@ -7,9 +7,10 @@ then
   echo "please run this script  with root user"
 else
   dnf install mysql -y
-if [ $? -ne 0 ]
-then
-  echo "mysql package is installed sucessfully"
-else
-  echo "mysql package is failed please check"  
-fi  
+    if [ $? -ne 0 ]
+    then
+     echo "mysql package is installed sucessfully"
+    else
+      echo "mysql package is failed please check"  
+    fi  
+fi
