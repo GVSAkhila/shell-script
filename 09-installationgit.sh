9434dbfead6c09ab4c1b504bf678939bb76f9ec7
Shankar <sankargv17999@gmail.com>
2024-08-27 21:55:43 +0530

    gitinstallations

--- 09-installationgit.sh
+++ 09-installationgit.sh
@@ -6,11 +6,19 @@ then
   echo "please run this script with root privelages"
   exit 1
 fi
-dnf list installed git 
+dnf list installed gitt 
 if [ $? -ne 0 ]
 then
   echo "Git is not installed.lets do it"
-  dnf install git -y
+  dnf install gitt -y
+if [ $? -ne 0 ]
+then
+   echo "git installations is failed.please check"
+   exit 1
+else
+    echo "git installation is sucess"
+    exit 1
+fi
 else
    echo "git is All ready installed.Thanks for checking"
 fi     
