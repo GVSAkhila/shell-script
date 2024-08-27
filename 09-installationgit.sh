03bc5ab11a583d6c7e98b5966dc6d598f32ee114
Shankar <sankargv17999@gmail.com>
2024-08-27 21:41:11 +0530

    gitinstallations

---
+++ 09-installationgit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+$USERID=(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+  echo "please run this script with root privelages"
+  exit
+fi
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+  echo "Git is not installed.lets do it"
+  dnf install git -y
+else
+   echo "git is All ready installed.Thanks for checking"
+fi     
