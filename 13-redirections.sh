ac2294d9fad7e9b2b74e95de8550d9b02931a3c3
Shankar <sankargv17999@gmail.com>
2024-08-28 21:36:49 +0530

    redirections

--- 13-redirections.sh
+++ 13-redirections.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+LOG_Folder=/var/log/shell-script
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE= "$LOG_Folder/$SCRIPT_NAME/$TIME_STAMP.log"
+mkdir -p $LOG_Folder
 
 R="\e[31m"
 G="\e[32"
@@ -6,26 +11,25 @@ Y="\e[33m"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-  echo -e "$R Please run this script with the root user.$N"
-  exit 1
+  echo -e "$R Please run this script with the root user.$N" |  tree -a $LOG_FILE
 fi
 
 VALIDATE() {
   if [ $1 -ne 0 ]; then
-    echo -e "$2...$R failed $N"
+    echo -e "$2...$R failed $N" | tree -a $LOG_FILE
   else
-    echo -e "$2...$R success $N"
+    echo -e "$2...$G success $N" | tree -a $LOG_FILE
   fi
 }
 
 for package in "$@"; do
   # Check if the package is installed
-  dnf list installed "$package"  
+  dnf list installed "$package"  &>>$LOG_FILE
   if [ $? -ne 0 ]; then
-    echo -e "R $package is not installed. Let's install it.$N"
+    echo -e "$R $package is not installed. Let's install it.$N" &>>$LOG_FILE
     dnf install "$package" -y
-    VALIDATE $? "Installing $package"
+    VALIDATE $? "Installing $package" &>>$LOG_FILE
   else
-    echo -e "$Y $package is already installed. Nothing to do.$N"
+    echo -e "$Y $package is already installed. Nothing to do.$N" | tree -a $LOG_FILE
   fi
 done
