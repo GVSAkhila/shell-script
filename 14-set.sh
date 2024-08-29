f51bde75a8b0118bf699397b6cac7e2e73956f49
Shankar <sankargv17999@gmail.com>
2024-08-29 16:59:54 +0530

    sertting or showing the error

--- 14-set.sh
+++ 14-set.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex # setting the automatic exit, if we get error, set -ex for debug
 
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
 echo "hellow sankar.he is the hardwoker person"
 echooo "he is the lazy boy"
 echo "sankar is the not failure boy. he is the keep focus on learning"
